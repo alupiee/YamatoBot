466110bfdec0238ea6bb02e75b4886492d71d955
alupiee <87232003+alupiee@users.noreply.github.com>
2022-06-27 10:19:24 +0700

    Update __main__.py

--- ShuKurenaiXRoBot/__main__.py
+++ ShuKurenaiXRoBot/__main__.py
@@ -794,7 +794,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[𝐈 𝐀𝐦 𝐎𝐧𝐥𝐢𝐧𝐞](http://telegra.ph/file/ff558497b647d265ef029.jpg)", parse_mode=ParseMode.MARKDOWN,
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am totally online[!](https://telegra.ph/file/516cd9a83491aabed2ce9.jpg)", parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
                   [                  
