3e99600f47f2526dbe08c0699df94cc3fd8faad7
alupiee <87232003+alupiee@users.noreply.github.com>
2022-07-04 12:10:01 +0700

    Update myinfo.py

--- ShuKurenaiXRoBot/modules/myinfo.py
+++ ShuKurenaiXRoBot/modules/myinfo.py
@@ -8,15 +8,15 @@ from ShuKurenaiXRoBot import telethn as aasf
 from ShuKurenaiXRoBot.events import register 
 
 edit_time = 5
-Shu1 = "http://telegra.ph/file/3cd1f5776c3ea08f609f4.jpg"
-Shu2 = "http://telegra.ph/file/e384ab52db8c0912ca356.jpg"
-Shu3 = "http://telegra.ph/file/0adf9e97735ba8a420973.jpg"
-Shu4 = "http://telegra.ph/file/2861eda5afba02bf04254.jpg"
+Shu1 = "https://telegra.ph/file/bfa2a4c3a59659e1d3900.jpg"
+Shu2 = "https://telegra.ph/file/b2f07deb75997d7657315.jpg"
+Shu3 = "https://telegra.ph/file/4289b6a13927d27142d0e.jpg"
+Shu4 = "https://telegra.ph/file/ad381e58b667455105b5e.jpg"
 
 @register(pattern="/myinfo")
 async def proboyx(event):
   button = [[custom.Button.inline("CHECK",data="information")]]
-  on = await aasf.send_message(event.chat, f"**❦ Hᴇʏ {(event.sender.first_name)}**\n\n**❦ I Aᴍ [ShuKurenai](https://t.me/ShuKurenaiXRoBot)**\n**❦ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [ShuKurenaiTeam](t.me/shukurenai007)**", file=Shu1, buttons=button)
+  on = await aasf.send_message(event.chat, f"**❦ Hᴇʏ {(event.sender.first_name)}**\n\n**❦ I Aᴍ [Yamato](https://t.me/cutieepierobot)**\n**❦ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [Diitod Escobar](t.me/diitod)**", file=Shu1, buttons=button)
 
   await asyncio.sleep(edit_time)
   ok = await aasf.edit_message(event.chat_id, on, file=Shu2, buttons=button) 
@@ -44,7 +44,7 @@ async def callback_query_handler(event):
   try:
     boy = event.sender_id
     PRO = await bot.get_entity(boy)
-    SHU = "YOUR DETAILS BY SHU KURENAI \n"
+    SHU = "YOUR DETAILS BY Yamato Robot \n"
     SHU += f"FIRST NAME : {PRO.first_name} \n"
     SHU += f"LAST NAME : {PRO.last_name}\n"
     SHU += f"YOU BOT : {PRO.bot} \n"
